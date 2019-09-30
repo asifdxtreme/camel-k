467fcb1f2a1838299a65fb3f2b644256e1d505a7
asifdxtreme <mohammad.asif.siddiqui1@huawei.com>
2019-09-30 11:40:03 +0530

    Make KanikoBuildCache *Bool

--- pkg/cmd/install.go
+++ pkg/cmd/install.go
@@ -240,9 +240,9 @@ func (o *installCmdOptions) install(cobraCmd *cobra.Command, _ []string) error {
 		kanikoBuildCacheFlag := cobraCmd.Flags().Lookup("kaniko-build-cache")
 
 		if kanikoBuildCacheFlag.Changed {
-			platform.Spec.Build.KanikoBuildCache = o.kanikoBuildCache
+			*platform.Spec.Build.KanikoBuildCache = o.kanikoBuildCache
 		} else {
-			platform.Spec.Build.KanikoBuildCache = true
+			*platform.Spec.Build.KanikoBuildCache = true
 		}
 
 		platform.Spec.Resources.Kits = o.kits
